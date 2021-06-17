9469c566ddac4f6869dccdc21c5b4608376598bc
nicomarto <77281744+nicomarto@users.noreply.github.com>
2021-06-17 07:31:02 -0500

    SIR setup

---
+++ ISR Simulation model.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+S, I, R, alpha, T, days, C = [999], [1], [0], 0.00018, 14, 200, []
+for n in range(1,days):
+    S.append(S[n-1]-alpha*S[n-1]*I[n-1])
+    R.append(R[n-1]+I[n-1]/T)
+    I.append(I[n-1]+alpha*S[n-1]*I[n-1]-I[n-1]/T)
+    C.append(alpha*S[n-1]*I[n-1])
+plt.plot(S, label='Susceptible')
+plt.plot(I, label='Infected')
+plt.plot(R, label='Recovered')
+plt.plot(C, label='Daily Cases')
+plt.xlabel('Days')
+plt.ylabel('Population')
+plt.title('Pandemic Evolution (base case scenario)')
+plt.legend()
+plt.show()
+Ib=I
+maxIb = np.max(I)
+print('Max Infected Population:', maxIb)
+maxCb = np.max(C)
+print('Max Daily Cases:',maxCb)
+
+S, I, R, alpha, T, days, v, vr, e, C = [999], [1], [0], 0.00018, 14, 177, [0], 5, 0.6, []
+
+for n in range(1,days):
+    S.append(max(S[n-1]-alpha*S[n-1]*I[n-1]-vr*e,0))
+    R.append(R[n-1]+I[n-1]/T)
+    I.append(I[n-1]+alpha*S[n-1]*I[n-1]-I[n-1]/T)
+    v.append(min(v[n-1]+vr,1000))
+    C.append(alpha*S[n-1]*I[n-1])
+Iv=I
+maxIv = np.max(I)
+print('Max Infected Population:', maxIv)
+maxCv = np.max(C)
+print('Max Daily Cases:',maxCv)
